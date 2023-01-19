0ec10bc289233bc3d131758fa768cc2b58dae117
MultaniAdil07 <123075100+MultaniAdil07@users.noreply.github.com>
2023-01-19 17:29:40 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,11 @@
 import streamlit
 
 streamlit.title('my perants new helthy diner')
+
+streamlit.header('breakfast manu')
+
+streamlit.text('omega 3 & blueberry oatmeal')
+
+streamlit.text('kale,Spinach & Rocker Smoothie')
+
+streamlit.text('Hard boiled Free-Range Egg')
