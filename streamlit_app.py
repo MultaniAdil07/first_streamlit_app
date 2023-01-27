eb2a3c6a47330cd7faa6437ce42153568a2f92b6
MultaniAdil07 <123075100+MultaniAdil07@users.noreply.github.com>
2023-01-27 17:46:46 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -50,5 +50,9 @@ my_data_rows = my_cur.fetchall()
 streamlit.header("The Fruit load list contains")
 streamlit.dataframe(my_data_rows)
 
+streamlit.header('fruityvice fruit advice')
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
+
 
 
