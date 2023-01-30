f9aeee417129b2fd1ea81a93e2233d931cc7b1c7
MultaniAdil07 <123075100+MultaniAdil07@users.noreply.github.com>
2023-01-30 12:29:10 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,27 +5,18 @@ import snowflake.connector
 from urllib.error import URLError
 
 streamlit.title('my perants new helthy diner')
-
 streamlit.header('breakfast manu')
-
 streamlit.text('🥣omega 3 & blueberry oatmeal')
-
 streamlit.text('🥗kale,Spinach & Rocker Smoothie')
-
 streamlit.text('🐔Hard boiled Free-Range Egg')
-
 streamlit.text('🥑🍞Avocardo Tost')
-
 streamlit.title('Build Your Own Fruit Smooothie')
    
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")  
-
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
 fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
-
 fruits_to_show = my_fruit_list.loc[fruits_selected]
-
 streamlit.dataframe(fruits_to_show)
 
 #new section to display fruitvice api responce 
@@ -39,4 +30,5 @@ try:
       fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
       streamlit.dataframe(fruityvice_normalized)
 
-
+except URLError as e:
+   streamlit.error()
