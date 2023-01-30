99f1f15ae03680641b7b41a78b2e5d5d13a7577c
MultaniAdil07 <123075100+MultaniAdil07@users.noreply.github.com>
2023-01-30 12:26:04 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,8 +4,6 @@ import requests
 import snowflake.connector
 from urllib.error import URLError
 
-
-
 streamlit.title('my perants new helthy diner')
 
 streamlit.header('breakfast manu')
@@ -25,7 +23,9 @@ my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.co
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
 fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
+
 fruits_to_show = my_fruit_list.loc[fruits_selected]
+
 streamlit.dataframe(fruits_to_show)
 
 #new section to display fruitvice api responce 
@@ -38,7 +38,5 @@ try:
       fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
       fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
       streamlit.dataframe(fruityvice_normalized)
-                                        
-except URLError as e:
-    streamlit.error()
+
 
